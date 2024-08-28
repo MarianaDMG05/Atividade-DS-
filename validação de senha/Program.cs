00fcf3085e8604b3e82ceb6570a1025ec7eadfab
KauaFSB <kauaflp234@gmail.com>
2024-08-27 22:05:25 -0300

    melhorado o codigo de validação de senha

--- validação de senha/Program.cs
+++ validação de senha/Program.cs
@@ -7,45 +7,45 @@ public class Program
     {
         // Software de validação de senha
         Console.Write("Digite uma senha:");
-        String senha = Console.ReadLine();
+        String senha = ValidarSenha(Console.ReadLine());
         
         Console.Write("\nConfirme a senha:");
-        String confirmaSenha = Console.ReadLine();    
+        String confirmaSenha = ValidarSenha(Console.ReadLine());    
         
         // faz a validação
-        bool tamanhoValido = senha.Length >= 8;
-        bool contemNumero = senha.Any(char.IsDigit);
-        bool contemMaiuscula = senha.Any(char.IsUpper);
-        
-        // faz a validação da confirmação
-        bool tamanhoValido2 = confirmaSenha.Length >= 8;
-        bool contemNumero2 = confirmaSenha.Any(char.IsDigit);
-        bool contemMaiuscula2 = confirmaSenha.Any(char.IsUpper);
-        
-        
-        // testa se todos foram positivos
-        if (tamanhoValido && contemMaiuscula && contemNumero){
-            Console.WriteLine("Senha Valida!");
-        }else {
-            Console.WriteLine("Senha Invalida!");
-        }
-
-        // testa se todos foram positivos
-        if (tamanhoValido2 && contemMaiuscula2 && contemNumero2){
-            Console.WriteLine("Confirmação de Senha Valida!");
-        }else {
-            Console.WriteLine("Confirmação de Senha Invalida!");
-        }
-
-        if(senha == confirmaSenha){
-            Console.WriteLine("Senhas conferem!");
-        }else {
-            Console.WriteLine("Senhas não conferem!");
-        }
-
+        bool igual = ComparaTexto(senha, confirmaSenha);
+       
+        if (igual)
+            Console.WriteLine("\nArmazenando dados!");
+        else
+            Console.WriteLine("\nRefaça o processo");
     }
+    
+    private static string ValidarSenha(string texto)
+    {
+        if (texto.Length < 8)
+            Console.WriteLine("\nTamanho inválido!");
+        if (!texto.Any(char.IsDigit))
+            Console.WriteLine("\nNão possui números!");
+        if (!texto.Any(char.IsUpper))
+            Console.WriteLine("\nNão possui letras maiúsculas!");
+        if (!texto.Any(ch => !char.IsLetterOrDigit(ch)))
+            Console.WriteLine("\nNão possui caracteres especiais!");
 
-    private string ValidarSenha(){
-        
+        return texto;
+    }
+    
+    private static bool ComparaTexto(string senha, string confirmaSenha)
+    {
+        if (senha.Equals(confirmaSenha, StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine("\nSenhas conferem!");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("\nSenhas não conferem!");
+            return false;
+        }
     }
 }
